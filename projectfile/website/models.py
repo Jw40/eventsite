00a5f46e82e07d1064efba907b45a98275939f27
Gordon Wong <gordonwong76@gmail.com>
2021-10-11 16:31:14 +0800

    Update models.py

--- projectfile/website/models.py
+++ projectfile/website/models.py
@@ -2,21 +2,23 @@
 
 from . import db
 from datetime import datetime
-from flask_login import UserMixin
 
-
-class User(db.Model, UserMixin):
+#users class
+class User(db.Model):
     __tablename__='users' # good practice to specify table name
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
-    emailid = db.Column(db.String(100), index=True, nullable=False)
+    name = db.Column(db.String(100), index=True)
+    emailid = db.Column(db.String(100), index=True, nullable=False, unique = True)
+    #emailid = db.Column(db.String(100), index=True, primary_key=True)
 	#password is never stored in the DB, an encrypted password is stored
 	# the storage should be at least 255 chars long
     password_hash = db.Column(db.String(255), nullable=False)
 
     # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='user')
+    booking = db.relationship('Booking', backref='user')
 
+#events class
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
@@ -24,13 +26,29 @@ class Event(db.Model):
     description = db.Column(db.String(200))
     image = db.Column(db.String(400))
     price = db.Column(db.Numeric(8))
+    quota = db.Column(db.Integer, default = 1)
+    category = db.Column(db.String(20))
     # ... Create the Comments db.relationship
 	# relation to call destination.comments and comment.destination
-    comments = db.relationship('Comment', backref='events')
+    comments = db.relationship('Comment', backref='user')
+    booking = db.relationship('Booking', backref='events')
+    # one to one relationship with event status
+    event_status = db.relationship('Event_Status', back_populates="events", uselist = False)
+    #event_status = db.relationship('Event_Status', backref="events")
 
     def __repr__(self): #string print method
         return "<Name: {}>".format(self.name)
 
+#event_status class
+class Event_Status(db.Model):
+    __tablename__ = 'event_status'
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'),primary_key=True)
+    status = db.Column(db.String(10))
+
+    def __repr__(self):
+        return "<Status: {}".format(self.status)
+
+#comments class
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
@@ -44,5 +62,15 @@ class Comment(db.Model):
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
 
-#create Event_Status class
-#create Booking class
+
+#booking class
+class Booking(db.Model):
+    __tablename__ = 'booking'
+    booking_Id = db.Column(db.Integer, primary_key=True)
+    booking_date = db.Column(db.Date, default=datetime.now())
+    price = db.Column(db.Integer)
+    quantity = db.Column(db.Int, default = 1)
+    
+    #foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    events_id = db.Column(db.Integer, db.ForeignKey('events.id'))
