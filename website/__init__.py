4cf3506e646d3d9e0635a762697946e4a04c35b4
James Wyatt <n8040478@qut.edu.au>
2021-11-02 23:24:39 +1000

    Use env var for database

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+import os
 db=SQLAlchemy()
 
 #create a function that creates a web application
@@ -19,7 +19,7 @@ def create_app():
    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
    #set the app configuration data 
-   app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sitedata.sqlite'
+   app.config['SQLALCHEMY_DATABASE_URI']= os.environ['DATABASE_URL'] # change from 'sqlite:///sitedata.sqlite'
    #initialize db with flask app
    db.init_app(app)
 
