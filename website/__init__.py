d502972c8e1ce18236839552cddfd7c93fd3e7b6
James Wyatt <n8040478@qut.edu.au>
2021-11-03 00:39:42 +1000

    test

--- website/__init__.py
+++ website/__init__.py
@@ -19,8 +19,7 @@ def create_app():
    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
    #set the app configuration data 
-   app.config['SQLALCHEMY_DATABASE_URI']= os.environ['DATABASE_URL'] 
-   # change from 'sqlite:///sitedata.sqlite'
+   app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///sitedata.sqlite'
    #initialize db with flask app
    db.init_app(app)
 
