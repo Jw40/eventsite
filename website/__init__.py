90740ba46824fe5c841687bfc554bd0f2bfc8e4b
James Wyatt <n8040478@qut.edu.au>
2021-11-03 01:52:51 +1000

    postgres

--- website/__init__.py
+++ website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
 
    uri = os.getenv("DATABASE_URL")  # or other relevant config var
    if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+      uri = uri.replace("postgres://", "postgresql://", 1)
    #set the app configuration data 
    app.config['SQLALCHEMY_DATABASE_URI']= uri
    #app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///sitedata.sqlite'
