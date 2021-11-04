5c56e4047ca6212d36733566b9d92ddbc5f1307d
James Wyatt <n8040478@qut.edu.au>
2021-11-04 22:12:01 +1000

    testing 500 error

--- website/__init__.py
+++ website/__init__.py
@@ -69,7 +69,7 @@ def create_app():
    #only working locally not on herouku
    @app.errorhandler(500)
    def internal_server_error(e):
-      return render_template('error/500.html'),500
+      return render_template('500.html'),500
    return app
 
 
